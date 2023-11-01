3fa39f42ef31c185c705e6e40326d8dc00209e52
alexb <alex@akridata.ai>
2023-11-01 14:47:56 +1100

    comments

--- python_examples/visual_search.py
+++ python_examples/visual_search.py
@@ -30,14 +30,17 @@ client = AkriDEClient(sdk_config_dict=sdk_config_dict)
 job = client.get_job_by_name("data-explore".upper())
 print(f"Got job - {job.get_name()}")
 
-# Positive Image samples for similarity search
-POS_IMAGE_SAMPLES = ["../dataset/cat-dog-bird/dog/109.jpg", "../dataset/cat-dog-bird/dog/1073.jpg"]
-# Negative Image Samples for similarity search
-NEG_IMAGE_SAMPLES = ["../dataset/cat-dog-bird/cat/1076.jpg", "../dataset/cat-dog-bird/bird/1080.jpg"]
+# Positive Image samples for similarity search - you want results similar to them:
+pos_images = ["../dataset/cat-dog-bird/dog/109.jpg", "../dataset/cat-dog-bird/dog/1073.jpg"]
+# Negative Image Samples for similarity search - you don't want results similar to them:
+neg_images = ["../dataset/cat-dog-bird/cat/1076.jpg", "../dataset/cat-dog-bird/bird/1080.jpg"]
 
 # Ensure that the image paths used here match the image paths used for ingest
-positive_samples = [os.path.abspath(os.path.join(os.getcwd(), sample)) for sample in POS_IMAGE_SAMPLES]
-negative_samples = [os.path.abspath(os.path.join(os.getcwd(), sample)) for sample in NEG_IMAGE_SAMPLES]
+positive_samples = [os.path.abspath(os.path.join(os.getcwd(), sample)) for sample in pos_images]
+negative_samples = [os.path.abspath(os.path.join(os.getcwd(), sample)) for sample in neg_images]
+print("Pos and Neg examples:")
+print(positive_samples)
+print(negative_samples)
 
 spec = {'positive_samples': positive_samples, 'negative_samples': negative_samples}
 samples = client.get_job_samples(job, akride.JobContext.SIMILARITY_SEARCH, spec)
@@ -45,4 +48,5 @@ samples = client.get_job_samples(job, akride.JobContext.SIMILARITY_SEARCH, spec)
 thumbnails = client.get_thumbnail_images(samples=samples)
 print(f"Retrieved {len(thumbnails)} images")
 
-display_images(thumbnails, n_rows=4, n_cols=4, figure_w=5, figure_h=5, output_file_name="search.jpg")
+print("Displaying the results:")
+display_images(thumbnails, n_rows=4, n_cols=4, figure_w=5, figure_h=5, save_file="search.jpg")
