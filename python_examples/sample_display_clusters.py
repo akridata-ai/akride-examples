57abecf2ea6eff9d35120837faf08115f3f96fc7
alexb <alex@akridata.ai>
2023-11-01 14:01:42 +1100

    added main to code with functions

--- python_examples/sample_display_clusters.py
+++ python_examples/sample_display_clusters.py
@@ -26,33 +26,38 @@ def display_cluster_images(images: list, cluster_id: int,
     # plt.savefig(f"./{cluster_id}.jpg")  # Uncomment to save the image locally
 
 
-# Get the API_KEY from Data Explorer → Utilities → Get CLI/SDK config:
-sdk_config_dict = {
-  "saas_endpoint": "https://app.akridata.ai",
-  "api_key": "akridata-804b6140d095:kGgXfc2qbXrgso0f5cGzuynaCiLxLZ0fc6xvRs6eFBAu0Ykd",
-  "mode": "saas"
-}
-# Define the Data Explorer client side:
-client = AkriDEClient(sdk_config_dict=sdk_config_dict)
+def main():
+    # Get the API_KEY from Data Explorer → Utilities → Get CLI/SDK config:
+    sdk_config_dict = {
+      "saas_endpoint": "https://app.akridata.ai",
+      "api_key": "akridata-804b6140d095:kGgXfc2qbXrgso0f5cGzuynaCiLxLZ0fc6xvRs6eFBAu0Ykd",
+      "mode": "saas"
+    }
+    # Define the Data Explorer client side:
+    client = AkriDEClient(sdk_config_dict=sdk_config_dict)
 
-# Retrieve the job by name:
-job = client.get_job_by_name("data-explore".upper())
-print(f"Got job - {job.get_name()}")
+    # Retrieve the job by name:
+    job = client.get_job_by_name("data-explore".upper())
+    print(f"Got job - {job.get_name()}")
 
-# Get number of clusters to sample from:
-num_clusters = job.info.to_dict()["tunables_default"]["max_clusters"]
-print(f"Data has {num_clusters} clusters")
+    # Get number of clusters to sample from:
+    num_clusters = job.info.to_dict()["tunables_default"]["max_clusters"]
+    print(f"Data has {num_clusters} clusters")
 
-# Number of samples to get from each cluster:
-max_count = 5
+    # Number of samples to get from each cluster:
+    max_count = 5
 
-for cluster_id in range(1, num_clusters + 1):
-    # Set the cluster ID:
-    spec = {"cluster_id": cluster_id, "max_count": max_count}
-    # Get thumbnails of the chosen samples:
-    samples = client.get_job_samples(job, JobContext.CLUSTER_RETRIEVAL, spec)  # type: ignore
-    cluster_images = client.get_thumbnail_images(samples)
-    # display grid:
-    display_cluster_images(cluster_images, cluster_id)
+    for cluster_id in range(1, num_clusters + 1):
+        # Set the cluster ID:
+        spec = {"cluster_id": cluster_id, "max_count": max_count}
+        # Get thumbnails of the chosen samples:
+        samples = client.get_job_samples(job, JobContext.CLUSTER_RETRIEVAL, spec)  # type: ignore
+        cluster_images = client.get_thumbnail_images(samples)
+        # display grid:
+        display_cluster_images(cluster_images, cluster_id)
 
-print("Provided examples for each cluster")
+    print("Provided examples for each cluster")
+
+
+if __name__ == '__main__':
+    main()
